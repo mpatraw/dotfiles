a2e9c1eaf712b7117975da401ac54aaff8421c38
Michael Patraw <mpatraw@apartments247.com>
2015-11-17 17:21:59 -0800

    Change to symlink termite config directory not file.

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ ln -s $PWD/.bashrc $HOME
 ln -s $PWD/.vimrc $HOME
 ln -s $PWD/.gvimrc $HOME
 ln -s $PWD/.vim $HOME
-ln -s $PWD/.config/termite/config $HOME/.config/termite/config
+ln -s $PWD/.config/termite $HOME/.config/termite
 ln -s $PWD/.xinitrc $HOME
