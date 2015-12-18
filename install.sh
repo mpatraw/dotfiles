8b7b81f3e07a71257d4737e660a8ba42428f15a2
Michael Patraw <mpatraw@apartments247.com>
2015-12-18 09:07:18 -0800

    Add emacs configuration. Mostly for clojure.

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ ln -s $PWD/.bashrc $HOME
 ln -s $PWD/.vimrc $HOME
 ln -s $PWD/.gvimrc $HOME
 ln -s $PWD/.vim $HOME
+ln -s $PWD/.emacs.d $HOME
 ln -s $PWD/.config/termite $HOME/.config/termite
 ln -s $PWD/.xinitrc $HOME
